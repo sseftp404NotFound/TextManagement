c3fc431fc5e6b5e9c943641d8714fb2a66311c5b
Hoki <1714043771@qq.com>
2018-07-05 01:41:52 +0800

    User初始化，界面初始化

--- src/main/java/com/slsnotfound/textmanagement/Model/User.java
+++ src/main/java/com/slsnotfound/textmanagement/Model/User.java
@@ -1,4 +1,25 @@
 package com.slsnotfound.textmanagement.Model;
 
+import lombok.Data;
+
+@Data
 public class User {
+    private int uid;
+    private String username;
+    private int sex;
+    private int year;
+    private int month;
+    private int day;
+    private String password;
+    private String adress;
+    private String referrer;
+    private String industry;
+    private String committee;
+    private int role;
+    private int status;
+    private String company;
+    private String profession;
+    private String email;
+    private String wechat;
+
 }
