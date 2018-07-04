a2ef5d0f1f2b90e12921c15d6517f4682b622572
bornfreeonekiss3 <lidantong123@163.com>
2018-07-05 04:08:06 +0800

    UserDao 初始化

--- src/main/java/com/slsnotfound/textmanagement/Dao/UserDao.java
+++ src/main/java/com/slsnotfound/textmanagement/Dao/UserDao.java
@@ -1,4 +1,18 @@
 package com.slsnotfound.textmanagement.Dao;
+import com.slsnotfound.textmanagement.Model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
+@Mapper
 public interface UserDao {
+    @Select("Select uid,password From User;")
+    List<User> GetUser();
+
+    @Insert("Insert Into User (username,password,sex,address,tel,referrer,year,month,day,industry,committee) Values('username','password',sex,'address',tel,'referrer',year,month,day,'industry','committee')")
+    @Options(useGeneratedKeys = true, keyProperty = "uid")
+    int insert(User user);
 }
