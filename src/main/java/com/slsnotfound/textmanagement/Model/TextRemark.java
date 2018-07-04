642dbd26fef9bad6041eafff7ac35c9e068f9bad
yuuuuu <qiutianfengye0201@163.com>
2018-07-05 02:08:39 +0800

    yuuu

--- src/main/java/com/slsnotfound/textmanagement/Model/TextRemark.java
+++ src/main/java/com/slsnotfound/textmanagement/Model/TextRemark.java
@@ -1,4 +1,11 @@
 package com.slsnotfound.textmanagement.Model;
 
 public class TextRemark {
+    private int Rid;  //评论人编号
+    private String RName;   //评论人姓名
+    private int RCondition;  //评论状态
+    private String RText;     //评论内容
+    private int Ryear;   //评论年
+    private int Rmonth;//评论月
+    private int Rday;//评论日
 }
