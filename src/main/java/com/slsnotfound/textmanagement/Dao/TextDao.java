72cdd1d1204feaa2a6bfd80b8c6931464906cf9e
bornfreeonekiss3 <lidantong123@163.com>
2018-07-08 15:25:05 +0800

    更正

--- src/main/java/com/slsnotfound/textmanagement/Dao/TextDao.java
+++ src/main/java/com/slsnotfound/textmanagement/Dao/TextDao.java
@@ -11,7 +11,7 @@ public interface TextDao {
     @Select("Select * From Text;")
     List<Text> getAllText();
 
-    @Insert("Insert Into Text (tID，tName，tWriterID,tWriterName,tText，bDay，bMonth， bYear,eDay,eMonth,eYear) Values(tID，'tName'，tWriterID,'tWriterName','tText'，bDay，bMonth，bYear,eDay,eMonth,eYear)")
+    @Insert("Insert Into Text (tID，tName，tWriterID,tWriterName,tText，bDay，bMonth， bYear,eDay,eMonth,eYear) Values(#{tID}，#{tName}，#{tWriterID},#{tWriterName},#{tText}，#{bDay}，#{bMonth}，#{bYear},#{eDay},#{eMonth},#{eYear})")
     @Options(useGeneratedKeys = true, keyProperty = "tID")
     int insertText (Text text);
 }
