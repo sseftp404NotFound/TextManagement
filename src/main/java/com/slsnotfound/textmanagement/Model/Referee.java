b21411f0e23a7929796f58124b980dbed1532730
bornfreeonekiss3 <lidantong123@163.com>
2018-07-09 17:02:36 +0800

    增加model referee

---
+++ src/main/java/com/slsnotfound/textmanagement/Model/Referee.java
@@ -0,0 +1,11 @@
+package com.slsnotfound.textmanagement.Model;
+
+public class Referee {
+    private int Gid;  //管理员编号
+    private String GName;   //管理员姓名
+    private int Gtel;  //管理员电话
+    private String Gcompany; //管理员单位
+    private String reason;//推荐原因
+
+
+}
